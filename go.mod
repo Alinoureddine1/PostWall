b0615e0a6a865aed7ee5d86656e491442a5478c5
Ali Noureddine <ali-nd2001@outlook.com>
2023-04-30 14:17:07 -0400

    Added templates and basic html

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Alinoureddine1/PostWall
 
 go 1.20
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,21 @@
-package main 
+package main
 
-import(
+import (
 	"fmt"
+	"github.com/gorilla/mux"
+	"html/template"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Hello World")
-}
-func main(){
-	http.HandleFunc("/", handler)
+var templates *template.Template
+
+func main() {
+	templates = template.Must(template.ParseGlob("templates/*.html"))
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler).Methods("GET")
+	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
+}
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
 }
