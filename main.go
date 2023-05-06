6e23f352ca83165693c851714e513ab5bd726f9e
Ali Noureddine <ali-nd2001@outlook.com>
2023-05-05 22:36:16 -0400

    Created login functionalityand cookie generator for each session

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"html/template"
-	"net/http"
-
 	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"html/template"
+	"net/http"
 )
 
 var client *redis.Client
 var templates *template.Template
+var store = sessions.NewCookieStore([]byte("t0p-s3cr3t"))
 
 func main() {
 	client = redis.NewClient(&redis.Options{
@@ -19,6 +20,10 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", indexGethandler).Methods("GET")
 	r.HandleFunc("/", indexPosthandler).Methods("POST")
+	r.HandleFunc("/test", testGethandler).Methods("Get")
+
+	r.HandleFunc("/login", loginGethandler).Methods("GET")
+	r.HandleFunc("/login", loginPosthandler).Methods("POST")
 	fs := http.FileServer(http.Dir("./static/"))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
 	http.Handle("/", r)
@@ -37,4 +42,31 @@ func indexPosthandler(w http.ResponseWriter, r *http.Request) {
 	comment := r.PostForm.Get("comment")
 	client.LPush("comments", comment)
 	http.Redirect(w, r, "/", 302)
+}
+
+func loginGethandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "login.html", nil)
+}
+
+func testGethandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session")
+	untyped, ok := session.Values["username"]
+	if !ok {
+		return
+	}
+	username, ok := untyped.(string)
+	if !ok {
+		return
+	}
+	w.Write([]byte(username))
+}
+
+func loginPosthandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	username := r.PostForm.Get("username")
+	password := r.PostForm.Get("password")
+	session, _ := store.Get(r, "session")
+	session.Values["username"] = username
+	session.Values["password"] = password
+	session.Save(r, w)
 }
